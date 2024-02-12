949b98593c551e2591031fcb810eed0bf12ffb63
Wayne Graham <wgraham@clir.org>
2024-02-12 12:01:39 -0500

    Update to Bootstrap 5.3.2

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -5,7 +5,6 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid'
 
-
 // import bootstrap stylesheets directly from your JS
 // import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap-icons/font/bootstrap-icons.css"; // needs additional webpack config!
@@ -28,7 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
 		eventSources: [
 			{
 				googleCalendarId: 'c_26af52aacd6118099527e6d8da6725baa71a1b209c5319c7298f5254dd819af7@group.calendar.google.com',
-				color: '#2778c8'
+				// color: '#2778c8'
+				color: '#49a18f'
 			}
 		]
 	});
