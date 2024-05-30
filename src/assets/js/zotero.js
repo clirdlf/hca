b9d38ec819ecb4828e22a59a880c3941d22a77d5
Wayne Graham <wgraham@clir.org>
2024-05-30 09:55:29 -0400

    Pull resources from Zotero

--- src/assets/js/zotero.js
+++ src/assets/js/zotero.js
@@ -3,6 +3,6 @@ import api from 'zotero-api-client'
 
 const { default: api } = ZoteroApiClient;
 
-const response = await.api().library('user', 475425).collections('9KH9TNSJ').items().get();
+const response = await.api().library('user', ).collections('9KH9TNSJ').items().get();
 
 console.log(items.map(i => i.title));
